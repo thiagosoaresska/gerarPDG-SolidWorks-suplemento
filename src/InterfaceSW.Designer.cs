bd10dbc9e53a5d6be19d67a0a584fe42405af401
Thiago Maciel Soares <thiago.soares@ska.com.br>
2023-12-18 16:23:18 -0300

    Interface do suplemento

---
+++ src/InterfaceSW.Designer.cs
@@ -0,0 +1,46 @@
+﻿
+namespace SuplementoParaGerarPDFSolidWorks
+{
+    partial class InterfaceSW
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InterfaceSW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "InterfaceSW";
+            this.Size = new System.Drawing.Size(566, 798);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
